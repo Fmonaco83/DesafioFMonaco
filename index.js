6fa480f919a93019d66a65ddd6161d8597085ae2
fmonaco83 <fernandomartinmonaco@gmail.com>
2022-06-01 19:25:17 -0300

    v1

---
+++ index.js
@@ -0,0 +1,8 @@
+alert ( 'Bienvenido')
+
+let numero = +(prompt('Ingresa un numero')) //Se debe ingresar un numero para comenzar con la operacion.
+
+for (let k=0; k<10; k++) {   //al numero ingresado se le sumara 1 por cada vuelta (total 10 vueltas)
+    alert (k + numero)       // Nos indicara en patalla cada uno de los resultados 
+
+}
